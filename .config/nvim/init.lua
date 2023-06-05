18369875fdc9df881557b46af07aaf4946799baf
Istvan Szekeres <istvan.szekeres@five.ai>
2023-06-05 17:28:42 +0100

    work

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -2,6 +2,7 @@ vim.cmd [[colorscheme slate]]
 vim.wo.number = true
 vim.wo.signcolumn = "yes"
 vim.g.mapleader = ","
+vim.g.python3_host_prog = '/home/istvan/.pyenv/versions/py3nvim/bin/python'
 
 if vim.fn.executable("rg") > 0 then
   vim.o.grepprg = "rg --hidden --glob '!.git' --no-heading --smart-case --vimgrep --follow $*"
@@ -9,3 +10,7 @@ if vim.fn.executable("rg") > 0 then
 end
 
 require('plugins')
+require('treesitter')
+require('lsp')
+require('keys')
+
